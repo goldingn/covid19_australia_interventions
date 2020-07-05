9ae8e8697c02d204eaada17526131e814996f34e
Nick Golding <goldingn35@gmail.com>
2020-07-05 23:17:52 +0800

    predownload gi parameter samples

--- R/functions.R
+++ R/functions.R
@@ -725,7 +725,7 @@ nishiura_samples <- function () {
   # priors for the parameters of the lognormal distribution over the serial interval from Nishiura et
   # al., as stored in the EpiNow source code 
   read_csv(
-    file = "https://raw.githubusercontent.com/goldingn/EpiNow/master/data-raw/nishiura-lognormal-truncated.csv",
+    file = "data/parameters/nishiura_samples.csv",
     col_types = cols(param1 = col_double(),
                      param2 = col_double())
   )
