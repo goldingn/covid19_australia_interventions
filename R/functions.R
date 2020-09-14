274b57b7765c4c8075ccb0a48602c4d1bc63f0cf
Nick Golding <goldingn35@gmail.com>
2020-09-14 11:48:25 +0800

    update delay distribution bins

--- R/functions.R
+++ R/functions.R
@@ -3319,14 +3319,16 @@ notification_delay_group <- function(date_confirmation, state) {
   stage <- case_when(
     date_confirmation < as.Date("2020-06-14") ~ 1,
     date_confirmation < as.Date("2020-08-01") ~ 2,
-    TRUE ~ 3,
+    date_confirmation < as.Date("2020-08-21") ~ 3,
+    TRUE ~ 4,
   )
   
   group <- case_when(
-    stage == 1 ~ "first_outbreak",
-    stage == 2 & state == "VIC" ~ "vic_outbreak_1",
-    stage == 3 & state == "VIC" ~ "vic_outbreak_2",
-    TRUE ~ "other"
+    stage == 1 ~ "all states (start-Jun13)",
+    stage == 2 & state == "VIC" ~ "VIC 1 (Jun14-Jul31)",
+    stage == 3 & state == "VIC" ~ "VIC 2 (Aug1-Aug20)",
+    stage == 4 & state == "VIC" ~ "VIC 3 (Aug21-now)",
+    TRUE ~ "other states Jun14-now"
   )
   
   group
