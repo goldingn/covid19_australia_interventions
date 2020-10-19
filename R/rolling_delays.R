6111f5aca2d7129b64781ed4fe50ce25ebfa7de2
Nick Golding <goldingn35@gmail.com>
2020-10-19 08:34:13 +0800

    fix display of dates in rolling surveillance trend

--- R/rolling_delays.R
+++ R/rolling_delays.R
@@ -22,11 +22,6 @@ delay_data_from_onset <- detection_delay_data %>%
     date_onset <= (linelist_date - 15)
   )
 
-delay_data_to_confirmation <- detection_delay_data %>%
-  filter(
-    date_confirmation <= (linelist_date - 3)
-  )
-  
 delays_from_onset <- estimate_delays(
   state = delay_data_from_onset$state,
   date = delay_data_from_onset$date_onset,
@@ -34,23 +29,32 @@ delays_from_onset <- estimate_delays(
   direction = "forward"
 )
 
+
+delay_data_to_confirmation <- detection_delay_data %>%
+  filter(
+    date_confirmation <= (linelist_date - 3)
+  )
+
 delays_to_confirmation <- estimate_delays(
-  state = delay_data_from_onset$state,
-  date = delay_data_from_onset$date_confirmation,
-  delay = delay_data_from_onset$delay,
+  state = delay_data_to_confirmation$state,
+  date = delay_data_to_confirmation$date_confirmation,
+  delay = delay_data_to_confirmation$delay,
   direction = "backward"
 )
 
 saveRDS(delays_from_onset, "outputs/delay_from_onset_cdfs.RDS")
 saveRDS(delays_to_confirmation, "outputs/delay_to_confirmation_cdfs.RDS")
 
-p <- plot_delays(
-  delay_distributions = delays_from_onset,
-  date = delay_data_from_onset$date_onset,
-  state = delay_data_from_onset$state,
-  delay = delay_data_from_onset$delay,
-  ylim = c(-2, 20)
-) +
+p <- delays_from_onset %>%
+  filter(
+    date <= max(delay_data_from_onset$date_onset) 
+  ) %>%
+  plot_delays(
+    date = delay_data_from_onset$date_onset,
+    state = delay_data_from_onset$state,
+    delay = delay_data_from_onset$delay,
+    ylim = c(-2, 20)
+  ) +
   ggtitle(
     label = "Surveillance trend",
     subtitle = "Time from symptom onset to notification for locally-acquired cases"
