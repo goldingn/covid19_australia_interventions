c033e99fabdc8ccf0c171f1f5eb5bd3adb5aca20
Nick Golding <goldingn35@gmail.com>
2020-11-04 14:20:42 +0800

    add script for running historic reffs

--- R/functions.R
+++ R/functions.R
@@ -4422,7 +4422,7 @@ hard_clamp <- function(local_samples, target_date) {
 }
 
 # output simulations
-write_reff_sims <- function(fitted_model, dir = "outputs/projection") {
+write_reff_sims <- function(fitted_model, dir = "outputs/projection", write_reff_1 = TRUE) {
   
   # find the dates for clamping into the future (where 50%/95% cases so far detected)
   clip_idx_50 <- (fitted_model$data$detection_prob_mat > 0.5) %>%
@@ -4438,38 +4438,36 @@ write_reff_sims <- function(fitted_model, dir = "outputs/projection") {
   date_50 <- fitted_model$data$dates$infection[clip_idx_50]
   date_95 <- fitted_model$data$dates$infection[clip_idx_95]
   
-  reff_1 <- reff_sims(fitted_model, which = "R_eff_loc_1")
   reff_12 <- reff_sims(fitted_model, which = "R_eff_loc_12")
 
-  write_csv(
-    reff_1,
-    file.path(dir, "r_eff_1_local_samples.csv")
-  )
+  reff_12 %>%
+    write_csv(
+      file.path(dir, "r_eff_12_local_samples.csv")
+    )
   
-  write_csv(
-    reff_12,
-    file.path(dir, "r_eff_12_local_samples.csv")
-  )
+  reff_12 %>%
+    soft_clamp(date_50) %>%
+    write_csv(
+      file.path(dir, "r_eff_12_local_samples_soft_clamped_50.csv")
+    )
   
-  write_csv(
-    soft_clamp(reff_12, date_50),
-    file.path(dir, "r_eff_12_local_samples_soft_clamped_50.csv")
-  )
+  reff_12 %>%
+    soft_clamp(date_95) %>%
+    write_csv(
+      file.path(dir, "r_eff_12_local_samples_soft_clamped_95.csv")
+    )
   
-  write_csv(
-    soft_clamp(reff_12, date_95),
-    file.path(dir, "r_eff_12_local_samples_soft_clamped_95.csv")
-  )
+  if (write_reff_1) {
+    
+    reff_1 <- reff_sims(fitted_model, which = "R_eff_loc_1")
+    
+    reff_1 %>%
+    write_csv(
+      file.path(dir, "r_eff_1_local_samples.csv")
+    )
+    
+  }
   
-  write_csv(
-    hard_clamp(reff_12, date_50),
-    file.path(dir, "r_eff_12_local_samples_hard_clamped_50.csv")
-  )
-  
-  write_csv(
-    hard_clamp(reff_12, date_95),
-    file.path(dir, "r_eff_12_local_samples_hard_clamped_95.csv")
-  )
   
 }
 
---
+++ R/run_historic_reffs.R
@@ -0,0 +1,45 @@
+# run reff for past linelists
+source("R/functions.R")
+
+# list of linelist dates to run for
+sync_nndss()
+
+# those we want
+linelist_dates <- seq(as.Date("2020-04-01"), Sys.Date(), by = 7)
+
+# those that are available
+dates_available <- linelist_date_times("~/not_synced/nndss/") %>%
+  mutate(date = as.Date(date_time)) %>%
+  pull(date)
+
+keep <- linelist_dates %in% dates_available
+linelist_dates <- rev(linelist_dates[keep])
+
+for (i in seq_along(linelist_dates)) {
+  
+  # create output directory
+  date <- linelist_dates[i]
+  date_string <- as.character(date, format = "%Y%m%d")
+  
+  dir <- file.path("outputs/historic", date_string)
+  dir.create(dir, recursive = TRUE, showWarnings = FALSE)
+  
+  print(date_string)
+  
+  linelist <- load_linelist(date)
+  data <- reff_model_data(linelist)
+  
+  data$dates$linelist
+  
+  # save the key dates for Freya and David to read in, and tabulated local cases
+  # data for the Robs
+  write_reff_key_dates(data, dir)
+  write_local_cases(data, dir)
+  
+  # define the model (and greta arrays) for Reff, and sample until convergence
+  fitted_model <- fit_reff_model(data, max_tries = 0)
+  
+  # output Reff trajectory draws for Rob M
+  write_reff_sims(fitted_model, dir, write_reff_1 = FALSE)
+  
+}
