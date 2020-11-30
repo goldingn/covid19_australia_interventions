c1c57c11269e032aa3057ae0dcc3b2e68a519a0c
Nick Golding <goldingn35@gmail.com>
2020-11-30 16:26:25 +0800

    work around numerical underflow in convergence assessment

--- R/functions.R
+++ R/functions.R
@@ -3166,6 +3166,10 @@ convergence <- function(draws) {
   r_hats <- coda::gelman.diag(draws, autoburnin = FALSE, multivariate = FALSE)$psrf[, 1]
   n_eff <- coda::effectiveSize(draws)
   
+  # sometimes n_eff underflows to 0 simply because the values beinng traced are
+  # very small, so remove these (exactly 0 is not possible)
+  n_eff <- n_eff[n_eff != 0]
+  
   cat(sprintf("maximum R-hat: %.2f\nminimum n effective: %.2f",
               max(r_hats),
               min(n_eff)))
