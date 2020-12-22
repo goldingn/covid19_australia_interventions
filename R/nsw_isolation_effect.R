59ee111abdfc195e7721347cb049b1073f979cd6
Nick Golding <goldingn35@gmail.com>
2020-12-22 10:54:51 +0800

    compute additional isolation effect from NSW linelist

--- R/nsw_isolation_effect.R
+++ R/nsw_isolation_effect.R
@@ -1,79 +1,38 @@
 # estimate NSW isolation effect
 source("R/functions.R")
 
-# load and format data on time to isolation in NSW outbreak
-nsw_isolation_data <- read_csv(
-  "data/miscs/nsw_isolation_data.csv",
+nsw_ll <- read_csv(
+  "~/not_synced/nsw/20201222 - Case list - James McCaw.csv",
   col_types = cols(
-    notification_date = col_date(format = "%d/%m/%y"),
-    cases = col_double(),
-    symptomatic_at_test = col_double(),
-    isol_by_m2d = col_double(),
-    isol_by_1d = col_double(),
-    isol_by_2d = col_double(),
-    isol_by_3d = col_double(),
-    isol_after_3d = col_double()
+    .default = col_character(),
+    CASE_ID = col_double(),
+    EARLIEST_CONFIRMED_OR_PROBABLE = col_nsw_date(),
+    SYMPTOM_ONSET_DATE = col_nsw_date(),
+    CALCULATED_ONSET_DATE = col_nsw_date(),
+    AGE_AT_EVENT_YEARS = col_double(),
+    DATE_ISOLATION_BEGAN = col_nsw_date(),
+    SETTING_OF_TRANSMISSION_DATE = col_nsw_date(),
+    INTERVIEWED_DATE = col_nsw_date()
   )
 ) %>%
-  # replace the symptomatic at test column due to a data mismatch
   mutate(
-    symptomatic_at_test = isol_by_m2d + isol_by_3d + isol_after_3d
-  )
-
-# cumulative fractions in isolation by time since symptom onset
-cumulative_isolation_reporting <- nsw_isolation_data %>%
-  mutate(
-    prop_m2d = isol_by_m2d / symptomatic_at_test,
-    prop_1d = isol_by_1d / symptomatic_at_test,
-    prop_2d = isol_by_2d / symptomatic_at_test,
-    prop_3d = isol_by_3d / symptomatic_at_test
+    date_onset = SYMPTOM_ONSET_DATE,
+    time_to_isolation = as.numeric(DATE_ISOLATION_BEGAN - SETTING_OF_TRANSMISSION_DATE) - 5
   ) %>%
-  select(
-    date_confirmation = notification_date,
-    prop_m2d,
-    prop_1d,
-    prop_2d,
-    prop_3d
-  )
-
-# reweight these by dates of symptom onset
-cumulative_isolation_onset <- load_linelist() %>%
   filter(
-    state == "NSW" &
-      date_confirmation > as.Date("2020-12-10") &
-      import_status == "local"
-  ) %>%
-  select(
-    date_onset,
-    date_confirmation
-  ) %>%
-  right_join(
-    cumulative_isolation_reporting,
-    by = "date_confirmation"
-  ) %>%
-  group_by(date_onset) %>%
-  summarise_at(
-    vars(starts_with("prop")),
-    mean
+    !is.na(time_to_isolation) & !is.na(date_onset)
   )
 
-nsw_ecdfs <- cumulative_isolation_onset %>%
-  pivot_longer(
-    cols = starts_with("prop"),
-    names_to = "days",
-    values_to = "prop"
-  ) %>%
-  mutate(
-    days = case_when(
-      days == "prop_m2d" ~ -2,
-      days == "prop_1d" ~ 1,
-      days == "prop_2d" ~ 2,
-      days == "prop_3d" ~ 3
-    )
-  ) %>%
+
+nsw_ll$time_to_isolation
+
+
+
+
+nsw_ecdfs <- nsw_ll %>%
   group_by(date_onset) %>%
   summarise(
-    ecdf = list(make_ecdf(prop, days))
+    ecdf = list(ecdf(time_to_isolation))
   ) %>%
   arrange(
     desc(date_onset)
@@ -83,7 +42,7 @@ nsw_ecdfs <- cumulative_isolation_onset %>%
   )
 
 # convert to a reduction in Reff
-nsw_ecdfs$ecdf[[10]](-5:21)
+nsw_ecdfs$ecdf[[1]](-5:21)
 
 
 # do surveillance_effect using these CDFs, and do it using the statewide ones,
