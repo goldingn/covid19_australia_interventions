6ddf62d00edb69a56ea7035bcb3822cc908b4a09
Gerry Ryan <geryan@users.noreply.github.com>
2021-03-10 17:47:01 +1100

    use CV_SOURCE_INFECTION column and handle  changes to NNDSS data set

--- R/functions.R
+++ R/functions.R
@@ -3896,7 +3896,7 @@ get_nndss_linelist <- function(date = NULL, dir = "~/not_synced/nndss", strict =
   
   col_types <- NULL
   if (strict) {
-    col_types <- c(
+    col_types_1 <- c(
       STATE = "text",
       POSTCODE = "numeric",
       CONFIRMATION_STATUS = "numeric",
@@ -3920,14 +3920,62 @@ get_nndss_linelist <- function(date = NULL, dir = "~/not_synced/nndss", strict =
       CV_OTHER_COMORBIDITIES = "text",
       CV_GESTATION = "numeric",
       CV_CLOSE_CONTACT = "numeric"
+      #CV_EXPOSURE_SETTING = "numeric",
+      #CV_SOURCE_INFECTION = "numeric"
+    )
+    
+    
+    col_types_2 <- c(
+      STATE = "text",
+      POSTCODE = "numeric",
+      CONFIRMATION_STATUS = "numeric",
+      TRUE_ONSET_DATE = "date",
+      SPECIMEN_DATE = "date",
+      NOTIFICATION_DATE = "date",
+      NOTIFICATION_RECEIVE_DATE = "date",
+      Diagnosis_Date = "date",
+      AGE_AT_ONSET = "numeric",
+      SEX = "numeric",
+      DIED = "numeric",
+      PLACE_OF_ACQUISITION = "text",
+      HOSPITALISED = "numeric",
+      CV_ICU = "numeric",
+      CV_VENTILATED = "numeric",
+      OUTBREAK_REF = "text",
+      CASE_FOUND_BY = "numeric",
+      CV_SYMPTOMS = "text",
+      CV_OTHER_SYMPTOMS = "text",
+      CV_COMORBIDITIES = "text",
+      CV_OTHER_COMORBIDITIES = "text",
+      CV_GESTATION = "numeric",
+      #CV_CLOSE_CONTACT = "numeric"
+      CV_EXPOSURE_SETTING = "numeric",
+      CV_SOURCE_INFECTION = "numeric"
     )
   }
   
+  
+  ll_date <- data$date_time[[1]]
+  
+  
+  if(ll_date < "2021-03-08"){
+    col_types <- col_types_1
+  } else {
+    col_types <- col_types_2
+  }
   
   dat <- readxl::read_xlsx(
     data$file,
     col_types = col_types
   )
+  
+  if(ll_date < "2021-03-08"){
+    dat <- dat %>%
+      mutate(
+        CV_SOURCE_INFECTION = NA_real_
+      )
+  }
+  
   
   if (is.numeric(dat$POSTCODE)) {
     dat <- dat %>%
@@ -3952,11 +4000,28 @@ get_nndss_linelist <- function(date = NULL, dir = "~/not_synced/nndss", strict =
       SPECIMEN_DATE = clean_date(SPECIMEN_DATE)
     ) %>%
     mutate(
+      # import_status = ifelse(
+      #   is.na(PLACE_OF_ACQUISITION) |
+      #     grepl("^1101|^00038888", PLACE_OF_ACQUISITION),
+      #   "local",
+      #   "imported"
+      # )
       import_status = ifelse(
-        is.na(PLACE_OF_ACQUISITION) |
-          grepl("^1101|^00038888", PLACE_OF_ACQUISITION),
-        "local",
-        "imported"
+        test = grepl("^1101|^00038888", PLACE_OF_ACQUISITION),
+        yes = "local",
+        no = ifelse(
+          test = !is.na(PLACE_OF_ACQUISITION),
+          yes = "imported",
+          no = ifelse(
+            test = is.na(CV_SOURCE_INFECTION),
+            yes = "local",
+            no = ifelse(
+              test = CV_SOURCE_INFECTION == 1,
+              yes = "imported",
+              no = "local"
+            )
+          )
+        )
       )
     )
   
@@ -5430,7 +5495,7 @@ load_vic <- function (file) {
 load_linelist <- function(date = NULL,
                           use_vic = FALSE,
                           use_sa = FALSE,
-                          use_nsw = TRUE) {
+                          use_nsw = FALSE) {
   
   # load the latest NNDSS linelist (either the latest or specified file)
   linelist <- get_nndss_linelist(date = date)
