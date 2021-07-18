c8528df5c50a609f344cf88043ec720e72ccb3f6
geryan <geryan@users.noreply.github.com>
2021-07-19 09:34:26 +1000

    add processing for vaccination data and annotate pipeline

--- R/pipeline.R
+++ R/pipeline.R
@@ -9,32 +9,42 @@ source("R/functions.R")
 # 1. Ingest mobility data, fit mobility models and output figures
 # (<state>_datastream_model_fit.png x8 and multistate_model_fit.png), and trend
 # estimates (google_change_trends.RDS) [~60s]
-source("R/mobility_change.R")
 
+source("R/mobility_change.R")
+# -- These figs into dropbox/to Freya
+# -- outputs/mobility_dates.csv into dropbox/to Freya
 
 # 2. Vaccination effect
 # [~ 3 min]
 source("R/vacccination_effect.R")
+# -- Figs into dropbox / to Freya
 
 # Section B) Dependent on NNDSS data update:
-
 source("R/check_linelist.R")
+#  produces object `linelist_check`
+# explore `linelist_check %>% print(n=1500)` to check for cases missing 
+# acquisition information or other oddities
+# may be most useful to filter for or against NSW to check through
+# --  pass on summary to forecasting channel
+# -- check if missing_location_assumption in get_nndss_linelist needs to be altered
 
 # 3. Run surveillance effect models and output figures (surveillance_effect.png
 # and notification_delays.png) and model objects (delay_from_onset_cdfs.RDS)
 # [~60s]
 source("R/rolling_delays.R")
-
+#  -- figs to dropbox / to Freya
 
 # 4. Sync NNDSS data and write out case data (local_cases.csv) for Monash (Rob Hyndman/Mitch)
 # and U of Melbourne (Rob Moss/Ruarai). RNG seed needs to match that in R_effective.R for imputation to
 # be consistent. This is also done in the final script, but I send it to them
 # whilst waiting [~60s]
 set.seed(2020-04-29)
-sync_nndss()
+sync_nndss() # this line probably not necessary for HPC / widows, but shoudn't break aything
 data <- reff_model_data()
 data$dates$linelist  # check it synced properly
+# -- is this date the correct linelist date?
 write_local_cases(data)
+# -- put in dropbox and notify Mitch/Rob J/Ruarai
 
 # Check no entries classed as "ERROR" (i.e. conflicting PLACE_OF_ACQUISITION and CV_SOURCE_INFECTION)
 # if OK will only list "imported" and "local"
@@ -43,7 +53,8 @@ load_linelist() %>%
   table
 
 # write linelist format for UoAdelaide (Tobin/Josh) if necessary
-write_linelist()
+# usually unnecessary unless edits to raw linelist
+# write_linelist() 
 
 # Section C) Dependent on survey data update (the numbered file must be manually
 # copied to data/survey_raw):
@@ -59,6 +70,7 @@ ggsave(
   width = 9,
   height = 10
 )
+# will light up cells with any overly large counts. If this happens check raw data
 
 # 6. Output microdistancing survey data for U of Adelaide (Tobin/Josh, previously Dennis)
 # (data/microdistancing/Barometer wave <wave> compliance.csv and
@@ -66,12 +78,15 @@ ggsave(
 # model, and output figure (microdistancing_effect.png) and trend estimates
 # (microdistancing_trends.RDS) [~2 min]
 source("R/microdistancing_change.R")
+# -- email data/microdistancing/Barometer wave <wave> compliance.csv and
+# data/contacts/barometer/contact numbers wave <wave>.csv to Tobin/Dylan/Josh
+# -- figure to dropbox / Freya
 
 # 7. Run macrodistancing model and output figure ((microdistancing_effect.png) and
 # trend estimates (macrodistancing_trends.RDS). Can be run concurrently with
 # microdistancing model (i.e. on a separate process/machine) [~1-2h]
 source("R/macrodistancing_change.R")
-
+# -- figure to dropbox / Freya
 
 # Section C) Dependent on NNDSS data update and outputs of all previous scripts:
 
@@ -83,4 +98,7 @@ source("R/macrodistancing_change.R")
 # r_eff_12_local_samples.csv, r_eff_12_local_samples_soft_clamped_95.csv in both
 # outputs/ and outputs/projection/) [~1-2h]
 source("R/R_effective.R")
+## - figures, samples as above, plus wt, alpha and delta, and
+# outputs/output_dates.csv to dropbox/Freya
 
+# hooray - you're done!
---
+++ R/vaccination_data.R
@@ -0,0 +1,151 @@
+
+
+
+library(readxl)
+
+vaccine_files_dates <- function(
+  dir = "~/not_synced/vaccination/vaccination_data/",
+  name_pattern = " Individuals by Dose Number, Dose Type, and Demographics.xlsx"
+) {
+  files <- list.files(
+    path = "~/not_synced/vaccination/vaccination_data/",
+    pattern = ".xlsx$",
+    full.names = TRUE
+  )
+  
+  dates <- gsub(
+    pattern = " Individuals by Dose Number, Dose Type, and Demographics.xlsx",
+    replacement = "",
+    basename(files)
+  ) %>%
+    as.Date(format = "%Y.%m.%d")
+  
+  tibble::tibble(
+    file = files,
+    date = dates
+  )
+  
+}
+
+
+
+file <- vaccine_files_dates() %>%
+  pull(file)
+
+file <- file[2]
+
+read_vaccine_data <- function(
+  file
+){
+  
+  data_date <- sub(
+    pattern = " Individuals by Dose Number, Dose Type, and Demographics.xlsx",
+    replacement = "",
+    basename(file)
+  ) %>%
+    as.Date(format = "%Y.%m.%d")
+  
+  header <- readxl::read_xlsx(
+    path = file,
+    n_max = 2,
+    col_names = FALSE,
+    sheet = "Vaccine Brand Split"
+  ) %>%
+    t %>%
+    as_tibble %>%
+    tidyr::fill(V1, .direction = "down") %>%
+    mutate(name = paste(V1, V2, sep = "_")) %>%
+    pull(name)
+  
+  over_80 <- c(
+    "80+",
+    "80-84",
+    "85+",
+    "85-89",
+    "90+",
+    "90-94",
+    "95+",
+    "95-99",
+    "100+"
+  )
+  
+  readxl::read_xlsx(
+    path = file,
+    skip = 2,
+    col_names = header,
+    sheet = "Vaccine Brand Split",
+    n_max = 37
+  ) %>%
+    tidyr::fill(
+      `NA_Vaccine Name`,
+      .direction = "down"
+    ) %>%
+    dplyr::rename(
+      vaccine = `NA_Vaccine Name`,
+      age_class = `Administered State_Age at Encounter Date - 5 Year`
+    ) %>%
+    pivot_longer(
+      cols = -vaccine:-age_class,
+      names_to = "name",
+      values_to = "doses"
+    ) %>%
+    mutate(
+      state = sub(
+        pattern = "_.*",
+        replacement = "",
+        x = name
+      ),
+      dose_number = sub(
+        pattern = ".* ", # splits in trailing space - could get caught if format changes
+        replacement = "",
+        x = name
+      ) %>%
+        as.integer
+    ) %>%
+    dplyr::select(state, age_class, vaccine, dose_number, doses) %>%
+      filter(age_class != "Totals", state != "Totals") %>%
+    mutate(
+      vaccine = case_when(
+        vaccine == "COVID-19 Vaccine AstraZeneca" ~ "az",
+        TRUE ~ "pf"
+      )
+    ) %>%
+    pivot_wider(
+      names_from = dose_number,
+      values_from = doses
+    ) %>%
+    mutate(
+      `1` = `1` - `2`
+    ) %>%
+    pivot_longer(
+      cols = `1`:`2`,
+      names_to = "dose_number",
+      values_to = "doses"
+    ) %>%
+    rowwise %>%
+    mutate(
+      age_class = case_when(
+        any(age_class == over_80) ~ "80+",
+        TRUE ~ age_class
+      )
+    ) %>%
+    group_by(state, age_class, vaccine, dose_number) %>%
+    summarise(
+      doses = sum(doses)
+    ) %>%
+    ungroup %>%
+    mutate(
+      dose_number = as.integer(dose_number),
+      data_date = data_date,
+    )
+  
+
+}
+
+vaccine_data <- sapply(
+  X = vaccine_files_dates() %>%
+    pull(file),
+  FUN = read_vaccine_data,
+  simplify = FALSE
+) %>%
+  bind_rows
