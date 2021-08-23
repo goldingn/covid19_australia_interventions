aa5eab84584c2530cf327f999c5ee461c67b0f4b
Nick Golding <goldingn35@gmail.com>
2021-08-23 22:56:19 +0800

    use warm starts to improve speed and accuracy of get_R

--- R/functions.R
+++ R/functions.R
@@ -8570,15 +8570,23 @@ scrape_doses_timeseries <- function() {
 }
 
 
-get_R <- function (transition_matrix, stable_age = NULL, tolerance = 0.001, max_iter = 1000) {
+get_R <- function (transition_matrix, stable_age = NULL, tolerance = 0.001, max_iter = 1000, warm_start = TRUE) {
   #Re(eigen(x)$value[1])
   # function from STEPS
   # https://github.com/steps-dev/steps/blob/74c5359dd4470c4056cd799c53ef56d503ba69da/R/growth_transition_functions-class.R#L211
   # compute R from a transition (next generation) matrix
   
+  # assign the stab;e age distribution if not known
   if (is.null(stable_age)) {
-    stable_age <- rep(1, ncol(transition_matrix))
+    # if using warm starts, try to find the recent stashed stable age distribution
+    if (warm_start & exists(".ngm_stage_age_warm_start")) {
+      stable_age <- .ngm_stage_age_warm_start
+      # otherwise just start from scratch
+    } else {
+      stable_age <- rep(1, ncol(transition_matrix))
+    }
   }
+  
   old_stages <- stable_age
   converged <- FALSE
   iter <- 0
@@ -8600,6 +8608,11 @@ get_R <- function (transition_matrix, stable_age = NULL, tolerance = 0.001, max_
       max_iter,
       " iterations"
     )
+  }
+  
+  # globally assign the stable age distribution if needed
+  if (warm_start) {
+    .ngm_stage_age_warm_start <<- new_stages / sum(new_stages)
   }
   
   # return the intrinsic growth rate
