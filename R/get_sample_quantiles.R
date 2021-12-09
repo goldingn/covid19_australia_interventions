c8ed800eb27900f593afdc013a6e36c443cff9eb
geryan <geryan@users.noreply.github.com>
2021-12-10 09:35:52 +1100

    more sample quantile examples

--- R/get_sample_quantiles.R
+++ R/get_sample_quantiles.R
@@ -222,3 +222,128 @@ ggplot(c2_samples) +
   facet_wrap(~state) 
 
 
+c1_old <- read_reff_samples("outputs/projection_oldmatrix/r_eff_1_local_samples.csv") %>%
+  mutate(matrix = "old")
+c1_new <- read_reff_samples("outputs/projection/r_eff_1_local_samples.csv") %>%
+  mutate(matrix = "new")
+
+c12_old <- read_reff_samples("outputs/projection_oldmatrix/r_eff_12_local_samples.csv") %>%
+  mutate(matrix = "old")
+c12_new <- read_reff_samples("outputs/projection/r_eff_12_local_samples.csv") %>%
+  mutate(matrix = "new")
+
+
+c1 <- bind_rows(c1_old, c1_new)
+c12 <- bind_rows(c12_old, c12_new)
+
+ggplot(c1) +
+  geom_ribbon(aes(x = date, ymin = lw5, ymax = up95, col = matrix), alpha = 0) +
+  geom_ribbon(aes(x = date, ymin = lw25, ymax = up75, fill = matrix), alpha = 0.25) +
+  geom_line(aes(x = date, y = med, linetype = matrix), alpha = 0.25) +
+  facet_wrap(~state, ncol = 2) +
+  scale_fill_manual(values = c(green, "grey80")) +
+  scale_colour_manual(values = c(green, "grey80")) +
+  theme_cowplot() +
+  geom_vline(xintercept = as.Date("2021-02-22"), col = "steelblue") +
+  scale_y_continuous(limits = c(0, 5))
+
+ggplot(c12) +
+  geom_ribbon(aes(x = date, ymin = lw5, ymax = up95, col = matrix), alpha = 0) +
+  geom_ribbon(aes(x = date, ymin = lw25, ymax = up75, fill = matrix), alpha = 0.25) +
+  geom_line(aes(x = date, y = med, linetype = matrix), alpha = 0.25) +
+  facet_wrap(~state, ncol = 2) +
+  scale_fill_manual(values = c(green, "grey80")) +
+  scale_colour_manual(values = c(green, "grey80")) +
+  theme_cowplot() +
+  geom_vline(xintercept = as.Date("2021-02-22"), col = "steelblue") +
+  scale_y_continuous(limits = c(0, 5))
+  
+
+vthist <- read_csv("outputs/vaccine_effect_timeseries_2021-11-07.csv") %>%
+  mutate(method = "Historic")
+vtmat <- read_csv("outputs/vaccine_effect_timeseries_2021-11-14.csv") %>%
+  mutate(method = "Contact\nmatrix\nupdate")
+vtdata <- readRDS("outputs/vaccine_effect_timeseries.RDS") %>%
+  mutate(method = "Data\nupdate")
+
+vt <- bind_rows(vthist, vtmat, vtdata) %>%
+  mutate(
+    method = method %>%
+      factor(
+        levels = c(
+          "Data\nupdate",
+          "Contact\nmatrix\nupdate",
+          "Historic"
+        )
+      )
+  )
+
+
+ggplot(vt) +
+  geom_line(
+    aes(
+      x = date,
+      y = effect,
+      #colour = state,
+      #alpha = method
+      colour = method
+    ),
+    size = 1.5,
+    alpha = 0.7
+  ) +
+  theme_classic() +
+  labs(
+    x = NULL,
+    y = "Change in transmission potential",
+    #col = "State",
+    #alpha = "Method"
+    col = "Method"
+  ) +
+  scale_x_date(
+    breaks = "1 month",
+    date_labels = "%b %Y"
+  ) +
+  ggtitle(
+    label = "Vaccination effect",
+    subtitle = "Change in transmission potential due to vaccination"
+  ) +
+  cowplot::theme_cowplot() +
+  cowplot::panel_border(remove = TRUE) +
+  theme(
+    strip.background = element_blank(),
+    axis.title.y.right = element_text(vjust = 0.5, angle = 90, size = font_size),
+    #legend.position = c(0.02, 0.135),
+    legend.text = element_text(size = 12),
+    axis.text = element_text(size = 8),
+    axis.text.x = element_text(angle = 270),
+    plot.title = element_text(size = font_size + 8),
+    plot.subtitle = element_text(size = font_size)
+  ) +
+  # scale_colour_manual(
+  #   values = c(
+  #     "darkgray",
+  #     "cornflowerblue",
+  #     "chocolate1",
+  #     "violetred4",
+  #     "red1",
+  #     "darkgreen",
+  #     "darkblue",
+  #     "gold1"
+  #   )
+  # ) +
+  scale_y_continuous(
+    position = "right",
+    limits = c(0, 1),
+    breaks = seq(0, 1, by = 0.2)
+  ) +
+  #scale_alpha_manual(values = c(0.5, 1, 0.2)) +
+  #scale_linetype_manual(values = c(1, 2, 3)) +
+  facet_wrap(~state, ncol = 2)
+
+ggsave(
+  filename = "outputs/figures/vaccination_effect_comparison_all.png",
+  dpi = dpi,
+  width = 1500 / dpi,
+  height = 1250 / dpi,
+  scale = 1.2
+)
