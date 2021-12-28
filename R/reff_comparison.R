6ccdf305e06f53f653811845a03a6df6543a0690
geryan <geryan@users.noreply.github.com>
2021-12-29 09:40:52 +1100

    add comparison plot of TP with and without vaccination, by David Price

---
+++ R/reff_comparison.R
@@ -0,0 +1,131 @@
+# # # # # # # # # # # # # # # # # # # # # # # # 
+# Plot multiple TP trajectories on same axes
+# 
+# David Price
+# Feb, 2021
+# 
+
+require(tidyverse)
+require(lubridate)
+source('~/Desktop/COVID19_Data/forecast_plot_functions.R')
+green <- RColorBrewer::brewer.pal(8, "Set2")[1]
+
+# ------------------------ #
+
+the.date <- "21Dec2021"
+vacc.start <- ymd("2021-02-22")
+
+reff1 <- read_csv(paste0("~/Desktop/COVID19_Data/r_eff_1_local_samples_",the.date,"_noVac.csv"),
+                  col_types =cols(
+                    .default = col_double(),
+                    date = col_date(format = ""),
+                    state = col_character(),
+                    date_onset = col_date(format = "")
+                  ))
+
+
+
+reff2 <- read_csv(paste0("~/Desktop/COVID19_Data/r_eff_1_local_samples_",the.date,"_Vac.csv"),
+                  col_types =cols(
+                    .default = col_double(),
+                    date = col_date(format = ""),
+                    state = col_character(),
+                    date_onset = col_date(format = "")
+                  )) 
+
+start.date <- ymd("2021-02-01")
+end.date <- read_csv(paste0("~/Desktop/COVID19_Data/output_dates_",the.date,".csv")) %>% select(latest_reff_date) %>% pull() + 1
+date.label.format <- "%e/%m"
+n.week.labels.panel <- 4
+n.week.ticks <- "1 weeks"
+
+# Create date objects for ticks/labels (e.g., show ticks every n.week.ticks, but label every n.week.labels.panel)
+dd <- format(seq.Date(ymd(start.date), end.date, by=n.week.ticks), date.label.format)
+dd.labs <- as.character(dd)
+dd.labs[!(dd.labs %in% dd.labs[(seq(length(dd.labs),1,by=-n.week.labels.panel))])] <- ""
+dd.labs <- gsub(pattern = "^0", replacement = "", x = dd.labs)
+dd.labs <- gsub(pattern = "/0", replacement = "/", x = dd.labs)
+dd <- seq.Date(ymd(start.date), end.date, by=n.week.ticks)
+
+# Quantiles
+qs <- c(0.05, 0.25, 0.5, 0.75, 0.95)
+
+r1 <- reff1 %>% 
+  reshape2::melt(id.vars = c("date","state","date_onset")) %>%
+  group_by(date,state) %>% 
+  summarise(x = quantile(value, qs), q = qs) %>% 
+  reshape2::dcast(state+date ~ q, value.var = "x") %>%
+  rename("L90"="0.05", "L50"="0.25", "med"="0.5", "U50"="0.75", "U90"="0.95") %>% filter(date <= end.date)
+  
+
+r2 <- reff2 %>% 
+  reshape2::melt(id.vars = c("date","state","date_onset")) %>%
+  group_by(date,state) %>% 
+  summarise(x = quantile(value, qs), q = qs) %>% 
+  reshape2::dcast(state+date ~ q, value.var = "x") %>%
+  rename("L90"="0.05", "L50"="0.25", "med"="0.5", "U50"="0.75", "U90"="0.95")  %>% filter(date <= end.date)
+
+r2.post <- r2 %>% filter(date >= vacc.start)
+
+
+# Plot aesthetics
+outer.alpha <- 0.15
+inner.alpha <- 0.4
+line.alpha <- 0.8
+col1 <- "grey70"
+col2 <- green
+
+
+subset.states <- c("ACT","NSW","NT","QLD","SA","TAS","VIC","WA")
+# subset.states <- c("NSW")
+## Figure showing Reff VOC v non-VOC by state
+ggplot() +
+  geom_hline(yintercept = 1, linetype = "dotted") +
+  
+  geom_vline(
+    aes(xintercept = date),
+    data = interventions() %>% filter(state %in% subset.states),
+    colour = "grey80"
+  ) +
+  
+  geom_ribbon(data = r1 %>% filter(state %in% subset.states), aes(x = date, ymin = L90, ymax = U90), fill = col1, alpha=outer.alpha) +
+  geom_ribbon(data = r1 %>% filter(state %in% subset.states), aes(x = date, ymin = L50, ymax = U50), fill = col1, alpha=inner.alpha) +
+  geom_line(data = r1 %>% filter(state %in% subset.states), aes(x = date, y = L90), col = col1, alpha = line.alpha) +
+  geom_line(data = r1 %>% filter(state %in% subset.states), aes(x = date, y = U90), col = col1, alpha = line.alpha) +
+
+  geom_ribbon(data = r2.post %>% filter(state %in% subset.states), aes(x = date, ymin = L90, ymax = U90), fill = col2, alpha=outer.alpha) +
+  geom_ribbon(data = r2.post %>% filter(state %in% subset.states), aes(x = date, ymin = L50, ymax = U50), fill = col2, alpha=inner.alpha) +
+  geom_line(data = r2.post %>% filter(state %in% subset.states), aes(x = date, y = L90), col = col2, alpha = line.alpha) +
+  geom_line(data = r2.post %>% filter(state %in% subset.states), aes(x = date, y = U90), col = col2, alpha = line.alpha) +
+  
+  geom_vline(
+    data = prop_variant_dates() %>% filter(state %in% subset.states),
+    aes(xintercept = date),
+    colour = "firebrick1",
+    linetype = 5
+  ) +
+  
+  geom_vline(xintercept = vacc.start, colour = "steelblue3", linetype = 5) +
+  
+  facet_wrap(~state, ncol = 2, scales = "free") +
+
+  scale_y_continuous("", position = "right", breaks = seq(0,5,by=1)) +
+  scale_x_date("Date", breaks = dd, labels = dd.labs) +
+  # scale_x_date("Date", date_breaks = "1 month", date_labels = "%e/%m") +
+  
+  coord_cartesian(xlim = c(start.date, end.date),
+                  ylim = c(0, 5)) +
+  
+  cowplot::theme_cowplot() +
+  cowplot::panel_border(remove = TRUE) +
+  theme(legend.position = "none",
+        strip.background = element_blank(),
+        strip.text = element_text(hjust = 0, face = "bold"),
+        axis.title.y.right = element_text(vjust = 0.5, angle = 90),
+        # axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text.x = element_text(size = 9),
+        panel.spacing = unit(1.2, "lines"))
+
+ggsave(paste0("~/Desktop/COVID19_Data/reff_figures/r_eff_1_local_novacc_vacc_",the.date,".png"), height = 10, width = 9, bg = "white")
+
+
