eb8d31dd0f9ec609e4e2336f4a8aafbd34c02e04
geryan <geryan@users.noreply.github.com>
2022-03-04 17:41:39 +1100

    add `split_ticks_and_labels` function allows more ticks than labels in plot axis if desired.

--- R/functions.R
+++ R/functions.R
@@ -11750,3 +11750,55 @@ get_vaccine_transmission_effects <- function(ves, coverage) {
     )
   
 }
+
+
+split_ticks_and_labels <- function(
+  # data can be vector of dates or dataframe/tibble with date column
+  data,
+  tick_freq = "1 month",
+  label_freq = "2 months",
+  label_format = "%b %y",
+  label_last = TRUE
+){
+  
+  if(is.tbl(data)){
+    dates <- data$date
+  } else if (is.Date(data)){
+    dates <- data
+  } else {
+    stop ("Data must be data frame with `date` column or vector of dates")
+  }
+  
+  start_date <- min(dates)
+  end_date <- max(dates)
+  
+  # Create date objects for ticks/labels (e.g., show ticks every n.week.ticks, but label every n.week.labels.panel)
+  ticks <- seq.Date(
+    from = start_date,
+    to = end_date,
+    by = tick_freq
+  )
+  
+  labs_short <- seq.Date(
+    from = start_date,
+    to = end_date,
+    by = label_freq
+  ) %>%
+    format(label_format) %>%
+    as.character
+  
+  labs <- ticks %>%
+    format(label_format) %>%
+    as.character
+  
+  label_shift <- ifelse(label_last, 1, 0)
+  
+  labs[!(labs %in% labs_short) - label_shift] <- ""
+  
+  return(
+    list(
+      ticks = ticks,
+      labels = labs
+    )
+  )
+}
